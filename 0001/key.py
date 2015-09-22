e00beec2a3ca14595b3d4df20b03f2a545756232
jeff2ma <mjh2316@gmail.com>
2015-09-22 23:31:46 +0800

    finished 0001

---
+++ 0001/key.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# by JeffMa
+
+# 题目：做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），使用 Python 如何生成 200 个激活码（或者优惠券）？
+
+import random
+import os
+import string
+
+
+def GenKey(length):
+    chars = string.ascii_letters + string.digits
+    return ''.join([random.choice(chars) for i in range(length)])
+
+
+def SaveKey(content):
+    f = open('Result Key.txt', 'a')
+    f.write(content)
+    f.write('\n')
+    f.close()
+
+
+if __name__ == '__main__':
+    for i in range(20):
+        value = GenKey(20)
+        print value
+        SaveKey(value)
